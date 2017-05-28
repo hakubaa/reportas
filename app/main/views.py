324e5f04c4a2ab191cef86602bd55aeee0a6a3a9
kuba <jakub.agatowski@gmail.com>
2017-05-28 16:28:15 +0200

    Create template and views for displaying companies

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,23 @@
+from flask import render_template
+
+from app import db
 from app.main import main
+
+from db.models import Company
+
 
 @main.route("/", methods=["GET"])
 def index():
 	return "<h1>REPORTAS</h1>"
+
+
+@main.route("/companies", methods=["GET"])
+def companies():
+	data = db.session.query(Company).filter(Company.name != "").all()
+	return render_template("main/companies.html", data=data)
+
+
+@main.route("/companies/<isin>", methods=["GET"])
+def company(isin):
+	data = db.session.query(Company).filter_by(isin=isin).one()
+	return render_template("main/company.html", data=data)
