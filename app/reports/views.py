eb8e7706c0c60be610617364e518280419a3ef0a
kuba <jakub.agatowski@gmail.com>
2017-05-30 20:54:36 +0200

    Add reports blueprint and implement two views

---
+++ app/reports/views.py
@@ -0,0 +1,105 @@
+import os
+
+from flask import (
+	request, redirect, url_for, current_app, render_template, flash, abort
+)
+from werkzeug.utils import secure_filename
+import requests
+from sqlalchemy.orm.exc import NoResultFound
+
+from app import db
+from app.models import File
+from app.reports import reports
+from db.models import Company
+
+from parser.models import FinancialReport
+
+
+def allowed_file(filename):
+	exts = current_app.config.get("ALLOWED_EXTENSIONS")
+	return "." in filename and filename.rsplit(".", 1)[1].lower() in exts
+			
+
+@reports.route("/", methods=["GET"])
+def index():
+	return "<h3>Upload Report</h3>"
+
+
+@reports.route("/load", methods=["GET", "POST"])
+def load_report():
+	if request.method == "POST":
+
+		file = request.files.get("file")
+		if file and file.filename != "":
+			filename = secure_filename(file.filename)
+
+			if not allowed_file(filename):
+				flash("Not allowed extension")
+				return redirect(request.url)
+
+			path = os.path.join(current_app.config.get("UPLOAD_FOLDER"), 
+				                filename)
+			file.save(path)	
+
+		else:
+
+			if "url" in request.values:
+				url = request.values["url"]
+				filename = secure_filename(url.split("/")[-1])
+
+				if not allowed_file(filename):
+					flash("Not allowed extension")
+					return redirect(request.url)
+
+				response = requests.get(url, stream=True)
+				if response.status_code == 200:
+					path = os.path.join(current_app.config.get("UPLOAD_FOLDER"), 
+				                        filename)
+					with open(path, "wb") as f:
+						for chunk in response:
+							f.write(chunk)
+
+				else:
+					flash("Unable to load file from given url.")
+					return redirect(request.url)
+
+			else:
+				flash("No selected file")
+				return redirect(request.url)	
+
+		file_db = File(name=filename)
+		db.session.add(file_db)
+
+		return redirect(url_for("reports.parser"))
+
+	return render_template("reports/loader.html")
+
+
+@reports.route("/parser", methods=["GET"])
+def parser():
+	file_id = request.values.get("file_id")  
+	if not file_id:
+		abort(400) # BAD REQUEST
+
+	try:
+		file = db.session.query(File).filter_by(id=file_id).one()
+	except NoResultFound:
+		abort(404) # NOT FOUND
+
+	filepath = os.path.join(current_app.config["UPLOAD_FOLDER"], file.name)
+	if not os.path.exists(filepath): 
+		abort(500) # INTERNAL SERVER ERROR (no file)
+
+	report = FinancialReport(filepath)
+
+	# identify company in db
+	try: 
+		company = db.session.query(Company).\
+		              filter_by(isin=report.company["isin"]).one()
+	except (NoResultFound, AttributeError, KeyError):
+		company = None
+
+	import pdb; pdb.set_trace()
+
+	return render_template("reports/parser.html", report=report, 
+		                   company=company)
---
+++ tests/app/reports/test_views.py
@@ -0,0 +1,232 @@
+from io import BytesIO
+import os
+from unittest.mock import patch, Mock
+
+from flask import url_for, current_app
+
+from tests.app import AppTestCase
+
+from db.models import Company
+from app.models import File
+from app import db
+
+
+@patch("app.reports.views.FinancialReport")
+class ParserViewTest(AppTestCase):
+    
+	def create_fake_file(self, name="test.pdf", content=b"test"):
+		file = File(name=name)
+		db.session.add(file)
+		db.session.commit()
+		path = os.path.join(current_app.config.get("UPLOAD_FOLDER"), name)
+		with open(path, "wb") as f:
+			f.write(content)
+		return file
+
+	def create_fake_company(self, name="TEST", fullname="TEST COMPANY", 
+		                    isin="TEST"):
+		company = Company(name=name, fullname=fullname, isin=isin)
+		db.session.add(company)
+		db.session.commit()
+		return company
+
+	def test_for_creating_financial_document(self, report_mock):
+		file = self.create_fake_file()
+
+		response = self.client.get(
+			url_for("reports.parser"), data={"file_id": file.id}
+		)
+
+		self.assertTrue(report_mock.called_once)
+
+	def test_initialize_financial_document_with_proper_file(
+		self, report_mock
+	):
+		file = self.create_fake_file()
+
+		response = self.client.get(
+			url_for("reports.parser"), data={"file_id": file.id}
+		)
+
+		filepath = report_mock.call_args[0][0]
+		self.assertEqual(
+			filepath,
+			os.path.join(current_app.config.get("UPLOAD_FOLDER"), file.name)
+		)
+
+	def test_for_raising_404_when_file_id_does_not_exist(self, report_mock):
+		response = self.client.get(
+			url_for("reports.parser"), data={"file_id": 1}
+		)
+		self.assertEqual(response.status_code, 404)
+
+	def test_for_raising_400_when_no_file_id(self, report_mock):
+		file = self.create_fake_file()
+		response = self.client.get(url_for("reports.parser"))
+		self.assertEqual(response.status_code, 400)
+
+	def test_for_rendering_proper_template(self, report_mock):
+		file = self.create_fake_file()
+		response = self.client.get(
+			url_for("reports.parser"), data={"file_id": file.id}
+		)
+		self.assert_template_used("reports/parser.html")
+
+	def test_for_passing_report_to_template(self, report_mock):
+		temp_mock = Mock()
+		report_mock.return_value = temp_mock
+		file = self.create_fake_file()
+		response = self.client.get(
+			url_for("reports.parser"), data={"file_id": file.id}
+		)
+		report = self.get_context_variable("report")
+		self.assertEqual(report, temp_mock)
+
+	def test_for_raising_500_when_there_is_no_file_in_the_disc(self, rmock):
+		file = self.create_fake_file()
+		os.remove(os.path.join(current_app.config.get("UPLOAD_FOLDER"), 
+			                   file.name))
+		response = self.client.get(
+			url_for("reports.parser"), data={"file_id": file.id}
+		)
+		self.assertEqual(response.status_code, 500)
+
+	def test_for_passing_company_to_template(self, rmock):
+		file = self.create_fake_file()
+		company = self.create_fake_company()
+		fr = Mock()
+		fr.company = { "isin": company.isin }
+		rmock.return_value = fr
+
+		response = self.client.get(
+			url_for("reports.parser"), data={"file_id": file.id}
+		)
+
+		ctx_company = self.get_context_variable("company")
+		self.assertEqual(ctx_company, company)
+
+
+class TestLoadReportView(AppTestCase):
+
+	def tearDown(self):
+		super().tearDown()
+		# remove all files from uploads_test
+		folder = self.app.config.get("UPLOAD_FOLDER")
+		for file in os.listdir(folder):
+			file_path = os.path.join(folder, file)
+			try:
+				if os.path.isfile(file_path):
+					os.unlink(file_path)
+			except Exception as e:
+				pass
+
+	def test_get_request_renders_template(self):
+		response = self.client.get(url_for("reports.load_report"))
+		self.assert_template_used("reports/loader.html")
+
+	def test_for_creating_entry_in_database(self):
+		data = dict(
+			file=(BytesIO(b"Content of the file."), "report.pdf"),
+		)
+		response = self.client.post(
+			url_for("reports.load_report"), data=data, follow_redirects=True,
+			content_type="multipart/form-data"
+		)
+		self.assertEqual(db.session.query(File).count(), 1)
+		file = db.session.query(File).first()
+		self.assertEqual(file.name, "report.pdf")
+
+	def test_for_saving_file_to_disc(self):
+		data = dict(
+			file=(BytesIO(b"Content of the file."), "report.pdf"),
+		)
+		response = self.client.post(
+			url_for("reports.load_report"), data=data, follow_redirects=True,
+			content_type="multipart/form-data"
+		)
+		self.assertTrue(os.path.exists(
+			os.path.join(self.app.config["UPLOAD_FOLDER"], data["file"][1])
+		))
+
+	def test_for_showing_alert_when_no_file(self):
+		response = self.client.post(
+			url_for("reports.load_report"), follow_redirects=True,
+			content_type="multipart/form-data"
+		)
+		content = response.data.decode("utf-8")
+		self.assertIn("No selected file", content)
+
+	def test_for_showing_alert_when_file_without_name(self):
+		data = dict(
+			file=(BytesIO(b"Content of the file."), ""),
+		)
+		response = self.client.post(
+			url_for("reports.load_report"), data=data, follow_redirects=True,
+			content_type="multipart/form-data"
+		)
+		content = response.data.decode("utf-8")
+		self.assertIn("No selected file", content)
+
+	def test_for_showing_alert_when_file_with_not_allowed_extension(self):
+		data = dict(
+			file=(BytesIO(b"Content of the file."), "report.exe"),
+		)
+		response = self.client.post(
+			url_for("reports.load_report"), data=data, follow_redirects=True,
+			content_type="multipart/form-data"
+		)
+		content = response.data.decode("utf-8")
+		self.assertIn("Not allowed extension", content)
+
+	def test_for_redirecting_after_successful_read(self):
+		data = dict(
+			file=(BytesIO(b"Content of the file."), "report.txt"),
+		)
+		response = self.client.post(
+			url_for("reports.load_report"), data=data, follow_redirects=False,
+			content_type="multipart/form-data"
+		)
+		self.assertRedirects(response, url_for("reports.parser"))
+
+	@patch("app.reports.views.requests.get")
+	def test_for_downloading_file_from_external_webpage_when_url(
+		self, rget_mock
+	):
+		data = dict(url="http://localhost:5000/test.txt")
+		response = self.client.post(
+			url_for("reports.load_report"), data=data, follow_redirects=True,
+			content_type="multipart/form-data"
+		)
+		self.assertTrue(rget_mock.called)
+
+	@patch("app.reports.views.requests.get")
+	def test_for_showing_alert_when_invalid_url(self, rget_mock):
+		rmock = Mock()
+		rmock.status_code = 404
+		rget_mock.return_value = rmock
+		data = dict(url="http://localhost:5000/test.txt")
+		response = self.client.post(
+			url_for("reports.load_report"), data=data, follow_redirects=True,
+			content_type="multipart/form-data"
+		)	
+		content = response.data.decode("utf-8")
+		self.assertIn("Unable to load file from given url", content)
+
+	@patch("app.reports.views.requests.get")
+	def test_for_saving_file_from_external_webpage(self, rget_mock):
+		rmock = Mock()
+		rmock.status_code = 200
+		temp = Mock()
+		temp.return_value = iter([b"Test", b"report"])
+		setattr(rmock, "__iter__", temp)
+		rget_mock.return_value = rmock
+
+		data = dict(url="http://localhost:5000/test.txt")
+		response = self.client.post(
+			url_for("reports.load_report"), data=data, follow_redirects=True,
+			content_type="multipart/form-data"
+		)	
+		self.assertTrue(os.path.exists(
+			os.path.join(self.app.config["UPLOAD_FOLDER"], 
+				         data["url"].split("/")[-1])
+		))
