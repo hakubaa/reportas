1c6246db1b0e2ffac16e422173d1840ce63ce209
kuba <jakub.agatowski@gmail.com>
2017-10-15 21:09:33 +0200

    Remove series from chart

--- app/static/js/charts.js
+++ app/static/js/charts.js
@@ -36,6 +36,34 @@ BarChart.prototype.getDefaultOptions = function() {
     };
 }
 
+BarChart.prototype.appendDataset = function(data, label, id) {
+    if (this.indexOfDataset(id) >= 0) {
+        throw "Dataset ID in use.";
+    } else {
+        this.updateLabels(this.extractLabels(data));
+        var dataset = this.createDataset({
+            data: this.extractData(data), 
+            label: label, id: id
+        });
+        this.updateDatasets(dataset);
+        this.chart.update();
+    }
+}
+
+BarChart.prototype.updateLabels = function(labels) {
+    if (labels === undefined) labels = [];
+    labels = distinct(this.labels.concat(labels));
+    labels = labels.concat(this.createMissingLabels(labels));
+    this.labels = labels.sort(function(x, y) { 
+        return moment(x) - moment(y); 
+    });
+}
+
+// TODO: create labels for missing timestamps
+BarChart.prototype.createMissingLabels = function(labels) {
+    return [];
+}
+
 BarChart.prototype.extractLabels = function(data) {
     var labels = data.map(function(item) { return item.timestamp; });
     return labels;
@@ -53,34 +81,6 @@ BarChart.prototype.extractData = function(data) {
         });
     return data;    
 }
-
-BarChart.prototype.appendDataset = function(data, label) {
-    this.labels = this.labels.concat(this.extractLabels(data));
-
-    var data = this.extractData(data);
-    this.appendMissingDataPoints(data, this.labels);
-    data = this.sortDataPoints(data);
-
-    this.datasets.push(this.createDataset({data: data,label: label}));
-    
-    this.adjustLabels();
-    this.adjustDatasets();
-    this.chart.update();
-}
-
-BarChart.prototype.appendMissingDataPoints = function(data, labels) {
-    var dp_labels = data.map(function(item) { return item.x; });
-    for(var i = 0; i < labels.length; i++) {
-        if (dp_labels.indexOf(labels[i]) < 0) {
-            data.push({"y": null, "x": this.labels[i]});
-        }
-    }
-}
-
-BarChart.prototype.sortDataPoints = function(data) {
-    return data.sort(function(x, y) { return moment(x.x) - moment(y.x) });
-}
-
 
 BarChart.prototype.createDataset = function(config) {
     var options = {
@@ -102,26 +102,132 @@ BarChart.prototype.createBackgroundColors = function(data) {
     });
 }
 
-BarChart.prototype.adjustLabels = function() {
-    var labels = distinct(this.labels);
-    labels = labels.concat(this.createMissingLabels(labels));
-    this.labels = labels.sort(function(x, y) { 
-        return moment(x) - moment(y); 
-    });
-}
-
-BarChart.prototype.createMissingLabels = function(labels) {
-    return [];
-}
-
-BarChart.prototype.adjustDatasets = function() {
+BarChart.prototype.updateDatasets = function(dataset) {
+    if (dataset !== undefined) {
+        this.datasets.push(dataset);
+    }
     for(var i = 0; i < this.datasets.length; i++) {
         var data = this.datasets[i].data;
-        this.appendMissingDataPoints(data, this.labels);
+        this.appendMissingDataPoints(data);
         data = this.sortDataPoints(data);
         this.datasets[i].data = data;
         this.datasets[i].backgroundColor = this.createBackgroundColors(data);
     }
+}
+
+BarChart.prototype.appendMissingDataPoints = function(data) {
+    // Serialise x-ticks (timestamps - dates) to numbers in order to enable 
+    // to search them for missing timestamps that have to be add.
+    var dp_labels = data.map(function(item) { return item.x; }).map(Number);
+    for(var i = 0; i < this.labels.length; i++) {
+        if (dp_labels.indexOf(+this.labels[i]) < 0) { // '+' - serialise
+            data.push({"y": null, "x": this.labels[i]});
+        }
+    }
+}
+
+BarChart.prototype.sortDataPoints = function(data) {
+    return data.sort(function(x, y) { return moment(x.x) - moment(y.x) });
+}
+
+BarChart.prototype.update = function() {
+    this.removeEmptyMarginalDataPoints();
+    this.chart.update();
+}
+
+BarChart.prototype.removeEmptyMarginalDataPoints = function() {
+    this.removeLeftSideEmptyDataPoints();
+    this.removeRightSideEmptyDataPoints();
+}
+
+BarChart.prototype.removeLeftSideEmptyDataPoints = function() {
+    var emptyDataPoints = this.filterLeftSideDataPoints(
+        this.identifyEmptyDataPoints()
+    );
+    this.removeDataPoints(emptyDataPoints.sort().reverse());
+}
+
+BarChart.prototype.filterLeftSideDataPoints = function(indexes) {
+    var lefti = [];
+    for (var i = 0; i < indexes.length; i++) {
+        if (indexes.indexOf(i) < 0) {
+            break;
+        } 
+        lefti.push(i);
+    }
+    return lefti;
+}
+
+BarChart.prototype.identifyEmptyDataPoints = function() {
+    var valuesTrace = new Array(this.labels.length).fill(0);
+    for (var i = 0; i < this.datasets.length; i++) {
+        for (var j = 0; j < this.datasets[i].data.length; j++) {
+            if (this.datasets[i].data[j].y !== null) {
+                valuesTrace[j] += 1;
+            }
+        }
+    }
+    var indexes = Array.apply(null, {length: this.labels.length})
+        .map(Number.call, Number) // range (0 to N)
+        .filter(function(item, index, array) {
+            return valuesTrace[index] === 0;
+        });
+    return indexes;
+}
+
+BarChart.prototype.removeDataPoints = function(indexes) {
+    for (var i = 0; i < indexes.length; i++) { // start from the end
+        this.removeDataPoint(indexes[i]);
+    }
+}
+
+BarChart.prototype.removeDataPoint = function(index) {
+    this.labels.splice(index, 1);
+    this.datasets.forEach(function(dataset, index) {
+        dataset.data.splice(0, 1); 
+    });
+}
+
+BarChart.prototype.removeRightSideEmptyDataPoints = function() {
+    var emptyDataPoints = this.filterRightSideDataPoints(
+        this.identifyEmptyDataPoints()
+    );
+    this.removeDataPoints(emptyDataPoints.sort().reverse());
+}
+
+BarChart.prototype.filterRightSideDataPoints = function(indexes) {
+    var righti = [];
+    for (var i = this.labels.length-1; i >= 0; i--) {
+        if (indexes.indexOf(i) < 0) {
+            break;
+        }
+        righti.push(i);
+    }
+    return righti;
+}
+
+
+BarChart.prototype.removeDataset = function(id) {
+    var index = this.indexOfDataset(id);
+    if (index < 0) {
+        throw "Dataset ID not found."; 
+    }
+    this.removeDatasetByIndex(index);
+    this.chart.update();
+}
+
+BarChart.prototype.indexOfDataset = function(id) {
+    var datasets = this.datasets;
+    for (var i = 0; i < datasets.length; i++) {
+        if (datasets[i].id === id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+BarChart.prototype.removeDatasetByIndex = function(index) {
+    this.datasets.splice(index, 1);
 }
 
 Object.defineProperty(BarChart.prototype, "labels", {
